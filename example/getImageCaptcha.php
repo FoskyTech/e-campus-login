00214b9cca58c953379e991b10550eff56ccdd00
FoskyM <i@fosky.top>
2025-01-10 21:58:26 +0800

    docs: update example `getImageCaptcha`

--- example/getImageCaptcha.php
+++ example/getImageCaptcha.php
@@ -6,6 +6,9 @@ use FoskyTech\ECampusLogin\Api;
 
 $deviceId = 'ym-' . md5(date('Y-m-d'));
 $securityToken = Api::getSecurityToken($deviceId);
-$imageCaptcha = Api::getImageCaptcha($deviceId, $securityToken);
-
-print_r($imageCaptcha);
+if ($securityToken['level'] !== 0) {
+    $imageCaptcha = Api::getImageCaptcha($deviceId, $securityToken);
+    print_r($imageCaptcha);
+} else {
+    print_r('不需要验证码');
+}
